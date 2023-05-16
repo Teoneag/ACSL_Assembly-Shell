d60a1f634f46cd28e113ddb1fc54c562e4a55d24
Teoneag <teodor.neagoe.climber@gmail.com>
2023-05-16 12:13:05 +0300

    first-commit-step-0

---
+++ acsl-assembly-shell-master/src/com/kylediaz/acsl_assembly_interpreter/CompilationException.java
@@ -0,0 +1,7 @@
+package com.kylediaz.acsl_assembly_interpreter;
+
+public class CompilationException extends Exception {
+    public CompilationException(String msg) {
+        super(msg);
+    }
+}
